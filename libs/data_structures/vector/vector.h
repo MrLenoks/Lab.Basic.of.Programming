04624420b81a8fe9fd28bdcd5434a7b1e9e8e1a1
MrLenoks <Mr.Lenoks@yandex.ru>
2024-05-30 23:44:38 +0400

    append push / pop functions

--- libs/data_structures/vector/vector.h
+++ libs/data_structures/vector/vector.h
@@ -25,4 +25,20 @@ void shrinkToFit(vector *v);
 //освобождает память, выделенную вектору.
 void deleteVector(vector *v);
 
+//проверка на то, является ли вектор пустым.
+bool isEmpty(vector *v);
+
+//проверка на то, является ли вектор полным.
+bool isFull(vector *v);
+
+//возвращает i-ый элемент вектора v.
+int getVectorValue(vector *v, size_t i);
+
+//добавляет элемент x в конец вектора v.
+void pushBack(vector *v, int x);
+
+//удаляет последний элемент из вектора.
+void popBack(vector *v);
+
+
 #endif
