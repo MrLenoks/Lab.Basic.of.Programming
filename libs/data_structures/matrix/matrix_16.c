57b8654b55906b56b041365efa2ea15bfec1b1f9
MrLenoks <Mr.Lenoks@yandex.ru>
2024-05-31 02:48:22 +0400

    second task

--- libs/data_structures/matrix/matrix_16.c
+++ libs/data_structures/matrix/matrix_16.c
@@ -12,4 +12,51 @@ void firstTask(matrix *m){
     swapRows(*m, maxPos, minPos);
 }
 
+// функция-критерий для сравнения строк по максимальному элементу
+int getMax(int* a, int n) {
+    assert(n > 0);
+
+    int max = a[0];
+
+    for (int i = 1; i < n; i++) {
+        if (a[i] > max) {
+            max = a[i];
+        }
+    }
+
+    return max;
+}
+
+//сортирует строки по неубывнию наибольших элементов строк
+void sortRowsByMaxElement(matrix m) {
+    int *max = (int*)malloc(sizeof(int) * m.nRows);
+
+    for (int i = 0; i < m.nRows; i++) {
+        max[i] = getMax(m.values[i], m.nCols);
+    }
+
+    for (int i = 0; i < m.nRows; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < m.nRows; j++) {
+            if (max[j] < max[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        if (i != minIndex) {
+            swap(&max[i], &max[minIndex]);
+            swapRows(m, i, minIndex);
+        }
+    }
+
+    free(max);
+}
+
+//Задание 2: упорядочивает строки матрицы по неубыванию наибольших элементов строк.
+void secondTask(matrix m){
+    for (int i = 0; i < m.nRows; i++) {
+        sortRowsByMaxElement(m);
+    }
+}
 
--- libs/data_structures/matrix/test_matrix_16.c
+++ libs/data_structures/matrix/test_matrix_16.c
@@ -132,4 +132,96 @@ void test_firstTask(){
     test_firstTask_maxMinOneLine();
 }
 
+void test_secondTask_squareMatrix(){
+    matrix m = createMatrixFromArray(
+            (int[]) {
+                    7, 1, 2,
+                    1, 8, 1,
+                    3, 2, 1
+            }, 3, 3);
+
+    matrix m_return = createMatrixFromArray(
+            (int[]) {
+                    3, 2, 1,
+                    7, 1, 2,
+                    1, 8, 1
+            }, 3, 3);
+
+    secondTask(m);
+
+    for (int i = 0; i < m.nRows; i++) {
+        for (int j = 0; j < m.nCols; j++) {
+            assert(m.values[i][j] == m_return.values[i][j]);
+        }
+    }
+
+    freeMemMatrix(&m);
+    freeMemMatrix(&m_return);
+}
+
+void test_secondTask_oneMax(){
+    matrix m1 = createMatrixFromArray(
+            (int[]) {
+                    0, 1, 2, 8,
+                    1, 8, 1, 4,
+                    3, 2, 1, 8
+            }, 3, 4);
+
+    matrix m1_return = createMatrixFromArray(
+            (int[]) {
+                    0, 1, 2, 8,
+                    1, 8, 1, 4,
+                    3, 2, 1, 8
+            }, 3, 4);
+
+    secondTask(m1);
+
+    for (int i = 0; i < m1.nRows; i++) {
+        for (int j = 0; j < m1.nCols; j++) {
+            assert(m1.values[i][j] == m1_return.values[i][j]);
+        }
+    }
+
+    matrix m2 = createMatrixFromArray(
+            (int[]) {
+                    9, 1, 2, 5,
+                    1, 8, 1, 4,
+                    3, 2, 9, 6
+            }, 3, 4);
+
+    matrix m2_return = createMatrixFromArray(
+            (int[]) {
+                    1, 8, 1, 4,
+                    9, 1, 2, 5,
+                    3, 2, 9, 6
+            }, 3, 4);
+
+    secondTask(m2);
+
+    for (int i = 0; i < m2.nRows; i++) {
+        for (int j = 0; j < m2.nCols; j++) {
+            assert(m2.values[i][j] == m2_return.values[i][j]);
+        }
+    }
+
+    freeMemMatrix(&m1);
+    freeMemMatrix(&m2);
+    freeMemMatrix(&m1_return);
+    freeMemMatrix(&m2_return);
+}
+
+void test_secondTask(){
+    test_secondTask_squareMatrix();
+    test_secondTask_oneMax();
+}
+
+
+void test(){
+    test_firstTask();
+    test_secondTask();
+}
+
+int main(){
+    test();
+}
 
