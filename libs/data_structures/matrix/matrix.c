731648d9e3d78c62dcb3084b6d32971ae86e63db
MrLenoks <Mr.Lenoks@yandex.ru>
2024-05-31 00:34:15 +0400

    implementation of functions for working with dynamic memory

---
+++ libs/data_structures/matrix/matrix.c
@@ -0,0 +1,39 @@
+#include <malloc.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "matrix.h"
+
+//размещает в динамической памяти матрицу размером nRows на nCols
+matrix getMemMatrix(int nRows, int nCols){
+    int **values = (int **) malloc(sizeof(int*) * nRows);
+
+    for (int i = 0; i < nRows; i++){
+        values[i] = (int *) malloc(sizeof(int) * nCols);
+    }
+
+    return (matrix){values, nRows, nCols};
+}
+
+//размещает в динамической памяти массив из nMatrices матриц размером nRows на nCols
+matrix *getMemArrayOfMatrices(int nMatrices, int nRows, int nCols){
+    matrix *ms = (matrix*) malloc(sizeof(matrix) * nMatrices);
+
+    for (int i = 0; i < nMatrices; i++) {
+        ms[i] = getMemMatrix(nRows, nCols);
+    }
+
+    return ms;
+}
+
+//освобождает память, выделенную под хранение матрицы m
+void freeMemMatrix(matrix *m){
+    free (m->values);
+}
+
+//освобождает память, выделенную под хранение массива ms из nMatrices матриц
+void freeMemMatrices(matrix *ms, int nMatrices){
+    for(int i = 0; i < nMatrices; i++) {
+        free (ms[i].values);
+    }
+}
+
